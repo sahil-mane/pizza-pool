c816492a82c6e2540fafb78b1dcba2ecdc8b0cba
sahil-mane <manesahil238@gmail.com>
2025-05-28 21:01:19 +0530

    add variable in env

--- src/libs/cloudinary.js
+++ src/libs/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
     // Configuration
     cloudinary.config({ 
-        cloud_name: 'dchitmmmx', 
-        api_key: '638525411819457', 
-        api_secret: '-frjg7W1RirISADR48Of5-flGyo' // Click 'View API Keys' above to copy your API secret
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });    // Upload an image
     
 export default cloudinary;  
